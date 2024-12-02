06e4a853a18f28b665e357ee8e5824c8f1267c45
Roy <shuwj81@daum.net>
2024-12-02 11:06:29 +0900

    🪛[chore]: DiConatiner 코드 리팩토링  #26
    * Di 코드 변경 및  Module 이랑  컨테이너 코드 등 추가 및 기존 Di 코드 간소화

---
+++ Attendance/Projects/App/Sources/Di/UseCaseModuleFactory.swift
@@ -0,0 +1,81 @@
+//
+//  UseCaseModuleFactory.swift
+//  DDDAttendance
+//
+//  Created by Wonji Suh  on 12/2/24.
+//
+
+import Foundation
+import DiContainer
+import Networkings
+
+struct UseCaseModuleFactory {
+  let registerModule = RegisterModule()
+  
+  private var useCaseDefinitions: [() -> Module] {
+    [
+      {
+        self.makeModule(FireStoreUseCaseProtocol.self) {
+          FireStoreUseCase(
+            repository: self.resolveOrDefault(
+              FireStoreRepositoryProtocol.self,
+              default: DefaultFireStoreRepository())
+          )}
+      },
+      
+      {
+        self.makeModule(QrCodeUseCaseProtocool.self) {
+          QrCodeUseCase(
+            repository: self.resolveOrDefault(
+              QrCodeRepositoryProtcool.self,
+              default: DefaultQrCodeRepository())
+          )}
+      },
+      
+      {
+        self.makeModule(OAuthUseCaseProtocol.self) {
+          OAuthUseCase(
+            repository: self.resolveOrDefault(
+              OAuthRepositoryProtocol.self,
+              default: DefaultOAuthRepository())
+          )}
+      },
+      
+      {
+        self.makeModule(AuthUseCaseProtocol.self) {
+          AuthUseCase(
+            repository: self.resolveOrDefault(
+              AuthRepositoryProtocols.self,
+              default: DefaultAuthRepository())
+          )}
+      },
+      
+      {
+        self.makeModule(SignUpUseCaseProtocol.self) {
+          SignUpUseCase(
+            repository: self.resolveOrDefault(
+              SignUpRepositoryProtcol.self,
+              default: DefaultSignUpRepository())
+          )}
+      },
+    ]
+  }
+  
+  func makeAllModules() -> [Module] {
+    useCaseDefinitions.map { $0() }
+  }
+  
+  private func makeModule<T>(
+    _ type: T.Type,
+    factory: @escaping () -> T
+  ) -> Module {
+    registerModule.makeModule(type, factory: factory)
+  }
+  
+  private func resolveOrDefault<T>(
+    _ type: T.Type,
+    default defaultFactory: @autoclosure @escaping () -> T
+  ) -> T {
+    DependencyContainer.live.resolveOrDefault(type, default: defaultFactory())
+  }
+}
